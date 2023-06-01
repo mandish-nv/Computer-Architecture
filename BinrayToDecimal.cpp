316c479e2c3446d6e37e04c2d3d12511c2e67ba4
mandish-nv <89140564+mandish-nv@users.noreply.github.com>
2023-06-01 14:54:25 +0545

    Add files via upload

---
+++ BinrayToDecimal.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+int binarytodecimal (long binarynum){
+	int decimalnum=0, temp=0, remainder;
+	
+	while(binarynum!=0){
+		remainder= binarynum%10;
+		binarynum=binarynum/10;
+		decimalnum=decimalnum+remainder*pow(2,temp);
+		temp++;
+	}
+	return decimalnum;
+}
+
+int main(){
+	long binarynum, decimalnum;
+	
+	printf("Enter abinary number: ");
+	
+}
+
